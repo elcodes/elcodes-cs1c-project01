77a49ebaf49c82c1d0388bc4bddbd53c15332fa9
elcodes <ellis95official@gmail.com>
2020-01-09 14:00:35 -0800

    Added my name to main

--- src/DemoGit.java
+++ src/DemoGit.java
@@ -2,7 +2,7 @@
  * Simple class to test submitting to GitHub repository.
  * @author Foothill College, Bita M
  *
- * Note: If you modified this file then update the author name with your: FirstName LastName
+ * Note: If you modified this file then update the author name with your: Ellis Sentoso
  * 
  * Reminder: Take a look at "Example Program Submission" in course Modules and follow the
  *           "Program Guidelines" to make sure your submission is complete.
@@ -11,11 +11,12 @@ public class DemoGit
 {
 
 	/**
-	 * Ellis Sentoso
+	 * Output my name.
+	 * @param args Not used
 	 */
 	public static void main(String[] args) 
 	{
-		System.out.println("Hello. My name is Ellis");
+		System.out.println("Hello. My name is Ellis Sentoso");
 
 	}
 
